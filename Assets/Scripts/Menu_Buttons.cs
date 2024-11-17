2d6bfa48704b6b2ce69753d06643b330265b501b
Rashaansmith2022 <113324350+Rashaansmith2022@users.noreply.github.com>
2024-11-17 13:48:53 -0500

    Added a Main Menu update in the future

---
+++ Assets/Scripts/Menu_Buttons.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Buttons : MonoBehaviour
+{
+    //method to load scene sample scene
+    public void Loadscene()
+    {
+        //loads the sample scene using the method called Load scene
+        SceneManager.LoadScene("SampleScene");
+
+    }
+     
+}
